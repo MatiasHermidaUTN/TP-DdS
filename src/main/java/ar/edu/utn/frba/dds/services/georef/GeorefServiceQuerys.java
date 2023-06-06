9bcb90736722dfcd9395e6899f81a31b738d9af3
MatiasHermidaUTN <mhermida@frba.utn.edu.ar>
2023-06-06 16:28:29 -0300

    AdapterGeoref v1

--- src/main/java/ar/edu/utn/frba/dds/services/georef/GeorefServiceQuerys.java
+++ src/main/java/ar/edu/utn/frba/dds/services/georef/GeorefServiceQuerys.java
@@ -7,26 +7,16 @@ import retrofit2.Call;
 import retrofit2.http.GET;
 import retrofit2.http.Query;
 
-public interface GeorefService {
-    @GET("ubicacion")
-    Call<ListadoDeProvincias> ubicacion(@Query("lat") int lat, @Query("lon") int lon);
+public interface GeorefServiceQuerys {
+    /*@GET("ubicacion")
+    Call<ListadoDeProvincias> ubicacion(@Query("lat") Float lat, @Query("lon") Float lon);*/
 
     @GET("provincias")
     Call<ListadoDeProvincias> provincias(@Query("campos") String campos, @Query("orden") String orden);
 
-    @GET("provincias")
-    Call<ListadoDeProvincias> provincias(@Query("campos") String campos, @Query("nombre") String nombre, @Query("orden") String orden);
-
     @GET("municipios")
     Call<ListadoDeMunicipios> municipios(@Query("provincia") int idProvincia, @Query("campos") String campos, @Query("max") int max, @Query("orden") String orden);
 
-    @GET("municipios")
-    Call<ListadoDeMunicipios> municipios(@Query("provincia") int idProvincia, @Query("campos") String campos, @Query("nombre") String nombre, @Query("max") int max, @Query("orden") String orden);
-
     @GET("departamentos")
     Call<ListadoDeDepartamentos> departamentos(@Query("provincia") int idProvincia, @Query("campos") String campos, @Query("max") int max, @Query("orden") String orden);
-
-    @GET("departamentos")
-    Call<ListadoDeDepartamentos> departamentos(@Query("provincia") int idProvincia, @Query("campos") String campos, @Query("nombre") String nombre, @Query("max") int max, @Query("orden") String orden);
-
 }
