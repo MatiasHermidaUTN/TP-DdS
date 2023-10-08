87fd848ee6ed4320612ddd6dbb075f303fa6e069
MatiasHermidaUTN <mhermida@frba.utn.edu.ar>
2023-10-08 19:44:16 -0300

    Ranking Impacto de Incidentes y su Test

--- tpa-grupo16-2023/src/main/java/ar/edu/utn/frba/dds/models/ranking/MayorImpactoProblematicas.java
+++ tpa-grupo16-2023/src/main/java/ar/edu/utn/frba/dds/models/ranking/MayorImpactoProblematicas.java
@@ -1,31 +1,44 @@
 package ar.edu.utn.frba.dds.models.ranking;
 
+import ar.edu.utn.frba.dds.models.calculadorImpactoDeIncidentes.AdapterCalcNivImpacto;
+import ar.edu.utn.frba.dds.models.incidentes.Incidente;
+import ar.edu.utn.frba.dds.models.incidentes.Prestacion;
+import ar.edu.utn.frba.dds.models.repositorios.RepoEntidad;
+import ar.edu.utn.frba.dds.models.repositorios.RepoIncidente;
+import ar.edu.utn.frba.dds.models.repositorios.RepoPrestacion;
 import ar.edu.utn.frba.dds.models.repositorios.reposDeprecados.RepoEntidadDeprecado;
+import ar.edu.utn.frba.dds.models.repositorios.reposDeprecados.RepoPrestacionDeprecado;
 import ar.edu.utn.frba.dds.models.serviciosPublicos.Entidad;
 
+import java.io.IOException;
 import java.time.LocalDateTime;
 import java.util.List;
 
 public class MayorImpactoProblematicas implements GeneradorRanking {
 
     public List<Entidad> generarRanking(LocalDateTime fechaDeSemana) {
-        //TODO entrega 4
-        // a mayor cantidad de miembros en una comunidad -> mayor impacto de un incidente sobre esa comunidad
-        // tenemos que distinguir entre usuarios y observadores.
+//        List<Entidad> listaDeEntidades = new RepoEntidad().buscarTodos();
+//        List<Prestacion> listaDePrestaciones = new RepoPrestacion().buscarTodos();
         List<Entidad> listaDeEntidades = RepoEntidadDeprecado.getInstancia().getListaEntidades();
+        List<Prestacion> listaDePrestaciones = RepoPrestacionDeprecado.getInstancia().getListaPrestaciones();
 
-        listaDeEntidades.sort((entidad1, entidad2) -> {
-            double promedioCierreEntidad1 = entidad1.calcularImpactoProblematicas(fechaDeSemana);
-            double promedioCierreEntidad2 = entidad2.calcularImpactoProblematicas(fechaDeSemana);
-            if (promedioCierreEntidad1 < promedioCierreEntidad2) {
-                return 1;
-            } else if (promedioCierreEntidad1 > promedioCierreEntidad2) {
-                return -1;
-            } else {
-                return 0;
-            }
-        });
+        System.out.println("entidades del repo:" + listaDeEntidades);
+        System.out.println();
+        System.out.println("prestaciones del repo:" + listaDePrestaciones);
+        System.out.println();
 
-        return listaDeEntidades;
+        List<Entidad> entidadesRankeadas = null;
+        try {
+            AdapterCalcNivImpacto adapterCalcNivImpacto = AdapterCalcNivImpacto.instancia();
+            entidadesRankeadas = adapterCalcNivImpacto.calcularNivelDeImpacto(listaDeEntidades, listaDePrestaciones, fechaDeSemana);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        entidadesRankeadas.sort((entidad1, entidad2) -> -1);
+        System.out.println("entidadesRankeadas: " + entidadesRankeadas);
+        System.out.println();
+
+        return entidadesRankeadas;
     }
 }
--- tpa-grupo16-2023/src/test/java/ar/edu/utn/frba/dds/RankingTest.java
+++ tpa-grupo16-2023/src/test/java/ar/edu/utn/frba/dds/RankingTest.java
@@ -1,7 +1,6 @@
 package ar.edu.utn.frba.dds;
 
-import ar.edu.utn.frba.dds.models.comunidades.TipoUsuario;
-import ar.edu.utn.frba.dds.models.comunidades.Usuario;
+import ar.edu.utn.frba.dds.models.comunidades.*;
 import ar.edu.utn.frba.dds.models.incidentes.EstadoIncidente;
 import ar.edu.utn.frba.dds.models.incidentes.Incidente;
 import ar.edu.utn.frba.dds.models.incidentes.Prestacion;
@@ -12,6 +11,7 @@ import ar.edu.utn.frba.dds.models.ranking.InformeSemanal;
 import ar.edu.utn.frba.dds.models.ranking.MayorImpactoProblematicas;
 import ar.edu.utn.frba.dds.models.ranking.MayorIncidentesReportados;
 import ar.edu.utn.frba.dds.models.ranking.MayorPromedioCierre;
+import ar.edu.utn.frba.dds.models.repositorios.*;
 import ar.edu.utn.frba.dds.models.repositorios.reposDeprecados.RepoEntidadDeprecado;
 import ar.edu.utn.frba.dds.models.repositorios.reposDeprecados.RepoIncidenteDeprecado;
 import ar.edu.utn.frba.dds.models.repositorios.reposDeprecados.RepoPrestacionDeprecado;
@@ -19,6 +19,7 @@ import ar.edu.utn.frba.dds.models.repositorios.reposDeprecados.RepoUsuarioDeprec
 import ar.edu.utn.frba.dds.models.serviciosPublicos.Entidad;
 import ar.edu.utn.frba.dds.models.serviciosPublicos.Establecimiento;
 import ar.edu.utn.frba.dds.models.serviciosPublicos.Servicio;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.time.LocalDateTime;
@@ -28,6 +29,14 @@ public class RankingTest {
     RepoPrestacionDeprecado repoPrestacion = RepoPrestacionDeprecado.getInstancia();
     RepoEntidadDeprecado repoEntidad = RepoEntidadDeprecado.getInstancia();
     RepoIncidenteDeprecado repoIncidente = RepoIncidenteDeprecado.getInstancia();
+
+    RepoEntidad repoEntidad2 = new RepoEntidad();
+    RepoEstablecimientos repoEstablecimientos2 = new RepoEstablecimientos();
+    RepoServicios repoServicios2 = new RepoServicios();
+    RepoPrestacion repoPrestacion2 = new RepoPrestacion();
+    RepoIncidente repoIncidente2 = new RepoIncidente();
+
+
 
     Entidad entidadA = new Entidad("Entidad A");
     Entidad entidadB = new Entidad("Entidad B");
@@ -54,6 +63,10 @@ public class RankingTest {
     Prestacion prestacionB1 = new Prestacion(establecimientoB, servicioB1);
     Prestacion prestacionB2 = new Prestacion(establecimientoB, servicioB2);
     Prestacion prestacionC1 = new Prestacion(establecimientoC, servicioC1);
+
+    @BeforeEach
+    public void initGeneral(){
+    }
 
     public void initRankingMayorIncidentesReportados(){
         RepoPrestacionDeprecado.agregarPrestacion(prestacionA1);
@@ -205,6 +218,147 @@ public class RankingTest {
         this.incidenteC2.setHorarioCierre(LocalDateTime.of(2023, 7, 24, 20, 45));
     }
 
+    public void initRankingImpacto(){
+        RepoPrestacionDeprecado.agregarPrestacion(prestacionA1);
+        RepoPrestacionDeprecado.agregarPrestacion(prestacionA2);
+        RepoPrestacionDeprecado.agregarPrestacion(prestacionB1);
+        RepoPrestacionDeprecado.agregarPrestacion(prestacionB2);
+        RepoPrestacionDeprecado.agregarPrestacion(prestacionC1);
+
+        RepoEntidadDeprecado.agregarEntidad(entidadA);
+        RepoEntidadDeprecado.agregarEntidad(entidadB);
+        RepoEntidadDeprecado.agregarEntidad(entidadC);
+
+        //seteos de ID, TODO borrar si se usan los repos de hibernate
+        entidadA.setId(1);
+        entidadB.setId(2);
+        entidadC.setId(3);
+
+        //comunidades y miembros para ranking
+        Comunidad comunidadA1 = new Comunidad("Comunidad A1");
+        Comunidad comunidadA2 = new Comunidad("Comunidad A2");
+        Comunidad comunidadB1 = new Comunidad("Comunidad B1");
+        Comunidad comunidadC1 = new Comunidad("Comunidad C1");
+
+        Perfil perfilA1 = new Perfil("Perfil A1", comunidadA1, TipoPerfil.NORMAL);
+        Perfil perfilA2 = new Perfil("Perfil A2", comunidadA2, TipoPerfil.NORMAL);
+        Perfil perfilB1 = new Perfil("Perfil B1", comunidadB1, TipoPerfil.NORMAL);
+        Perfil perfilB2 = new Perfil("Perfil B2", comunidadB1, TipoPerfil.NORMAL);
+        Perfil perfilC1 = new Perfil("Perfil C1", comunidadC1, TipoPerfil.NORMAL);
+        Perfil perfilC2 = new Perfil("Perfil C2", comunidadC1, TipoPerfil.NORMAL);
+
+        perfilA1.setTipoMiembro(TipoMiembro.AFECTADO);
+        perfilA2.setTipoMiembro(TipoMiembro.AFECTADO);
+        perfilB1.setTipoMiembro(TipoMiembro.AFECTADO);
+        perfilB2.setTipoMiembro(TipoMiembro.OBSERVADOR);
+        perfilC1.setTipoMiembro(TipoMiembro.AFECTADO);
+        perfilC2.setTipoMiembro(TipoMiembro.AFECTADO);
+
+        comunidadA1.agregarMiembros(perfilA1);
+        comunidadA2.agregarMiembros(perfilA2);
+        comunidadB1.agregarMiembros(perfilB1);
+        comunidadB1.agregarMiembros(perfilB2);
+        comunidadC1.agregarMiembros(perfilC1);
+        comunidadC1.agregarMiembros(perfilC2);
+
+        //establecimiento A
+        this.establecimientoA.setEntidad(entidadA);
+        this.establecimientoA.setNombre("Estación A");
+
+        this.servicioA1.setNombre("Baño");
+        this.servicioA2.setNombre("Ascensor");
+
+        this.incidenteA1.setObservaciones("El cubiculo 3 del baño no funciona");
+        this.incidenteA2.setObservaciones("El lavabo del baño no funciona");
+        this.incidenteA3.setObservaciones("El ascensor no frena en el piso 2");
+        this.incidenteA4.setObservaciones("El ascensor no sube hasta el piso 3");
+
+        this.prestacionA1.agregarIncidente(incidenteA1);
+        this.prestacionA1.agregarIncidente(incidenteA2);
+        this.prestacionA2.agregarIncidente(incidenteA3);
+        this.prestacionA2.agregarIncidente(incidenteA4);
+
+        this.incidenteA1.setHorarioApertura(LocalDateTime.of(2023, 7, 26, 12, 0));  //Miercoles 26/7 (prestacionA1)
+        this.incidenteA1.setHorarioCierre(LocalDateTime.of(2023, 7, 26, 12, 30));
+        this.incidenteA1.setEstado(EstadoIncidente.RESUELTO);
+        this.incidenteA2.setHorarioApertura(LocalDateTime.of(2023, 7, 26, 15, 0));  //Miercoles 26/7 (prestacionA1)(menos de 24hs pero anterior RESUELTO)
+        this.incidenteA3.setHorarioApertura(LocalDateTime.of(2023, 7, 26, 12, 0));  //Miercoles 26/7 (prestacionA2)
+        this.incidenteA3.setHorarioCierre(LocalDateTime.of(2023, 7, 26, 13, 0));
+        this.incidenteA3.setEstado(EstadoIncidente.RESUELTO);
+        this.incidenteA4.setHorarioApertura(LocalDateTime.of(2023, 7, 30, 12, 0));  //Domingo 30/7 (prestacionA2)(misma semana)
+
+        incidenteA1.setComunidad(comunidadA1);  //comunidad con 1 afectado
+        incidenteA2.setComunidad(comunidadA1);  //comunidad con 1 afectado
+        incidenteA3.setComunidad(comunidadA2);  //comunidad con 1 afectado
+        incidenteA4.setComunidad(comunidadA2);  //comunidad con 1 afectado
+        // 4 afectados * [ (30 + 60) t resolucion de incidentes + 2 incidentes no resueltos * CNF{1.5*60} ] = 1080
+
+        //establecimiento B
+        this.establecimientoB.setEntidad(entidadB);
+        this.establecimientoB.setNombre("Estación B");
+
+        this.servicioB1.setNombre("Rampa");
+        this.servicioB2.setNombre("Escalera Mecanica");
+
+        this.incidenteB1.setObservaciones("La rampa no se puede acceder");
+        this.incidenteB2.setObservaciones("La escalera mecanica no funciona");
+        this.incidenteB3.setObservaciones("La escalera mecanica esta parada");
+
+        this.prestacionB1.agregarIncidente(incidenteB1);
+        this.prestacionB2.agregarIncidente(incidenteB2);
+        this.prestacionB2.agregarIncidente(incidenteB3);
+
+        this.incidenteB1.setHorarioApertura(LocalDateTime.of(2023, 7, 25, 12, 0));  //Martes 25/7 (prestacionB1)
+        this.incidenteB2.setHorarioApertura(LocalDateTime.of(2023, 7, 25, 12, 0));  //Martes 25/7 (prestacionB2)
+        this.incidenteB3.setHorarioApertura(LocalDateTime.of(2023, 7, 18, 12, 0));  //Martes 18/7 (prestacionB2)(en otra semana)
+
+        incidenteB1.setComunidad(comunidadB1);  //comunidad con 1 afectado y 1 observador
+        incidenteB2.setComunidad(comunidadB1);  //comunidad con 1 afectado y 1 observador
+        incidenteB3.setComunidad(comunidadB1);  //comunidad con 1 afectado y 1 observador (pero este es de otra semana)
+        // 2 afectados * [ (0) t resolucion de incidentes + 2 incidentes no resueltos * CNF{1.5*60} ] = 360
+
+
+        //establecimiento C
+        this.establecimientoC.setEntidad(entidadC);
+        this.establecimientoC.setNombre("Estación C");
+
+        this.servicioC1.setNombre("Puerta Emergencia");
+
+        this.incidenteC1.setObservaciones("La puerta de emergencia no abre");
+        this.incidenteC2.setObservaciones("La puerta de emergencia no cierra");
+
+        this.prestacionC1.agregarIncidente(incidenteC1);
+        this.prestacionC1.agregarIncidente(incidenteC2);
+
+        this.incidenteC1.setHorarioApertura(LocalDateTime.of(2023, 7, 24, 12, 0));  //Lunes 24/7 (prestacionC1)
+        this.incidenteC2.setHorarioApertura(LocalDateTime.of(2023, 7, 24, 20, 0));  //Lunes 24/7 (prestacionC1)(menos de 24hs del anterior ABIERTO)
+
+        incidenteC1.setComunidad(comunidadC1);  //comunidad con 2 afectados
+        incidenteC2.setComunidad(comunidadC1);  //comunidad con 2 afectados
+        // 4 afectados * [ (0) t resolucion de incidentes + 2 incidentes no resueltos * CNF{1.5*60} ] = 720
+
+        //TODO guardo en el repo
+//        repoServicios2.guardar(servicioA1);
+//        repoServicios2.guardar(servicioA2);
+//        repoServicios2.guardar(servicioB1);
+//        repoServicios2.guardar(servicioB2);
+//        repoServicios2.guardar(servicioC1);
+//
+//        repoEstablecimientos2.guardar(establecimientoA);
+//        repoEstablecimientos2.guardar(establecimientoB);
+//        repoEstablecimientos2.guardar(establecimientoC);
+//
+//        repoEntidad2.guardar(entidadA);
+//        repoEntidad2.guardar(entidadB);
+//        repoEntidad2.guardar(entidadC);
+
+//        repoPrestacion2.guardar(prestacionA1);
+//        repoPrestacion2.guardar(prestacionA2);
+//        repoPrestacion2.guardar(prestacionB1);
+//        repoPrestacion2.guardar(prestacionB2);
+//        repoPrestacion2.guardar(prestacionC1);
+    }
+
     @Test
     public void primerRankingMayorPromedioCierre() {
         this.initRankingPromedio();
@@ -237,15 +391,16 @@ public class RankingTest {
 
     @Test
     public void tercerRankingMayorImpactoProblematicas() {
+        this.initRankingImpacto();
         LocalDateTime fechaDeSemana = LocalDateTime.of(2023, 7, 25, 12, 0);
         MayorImpactoProblematicas generadorRanking = new MayorImpactoProblematicas();
 
         List<Entidad> rankingEntidades = generadorRanking.generarRanking(fechaDeSemana);
         System.out.println("rankingMayorImpactoProblematicas (TODO en 4ta entrega): ");
         for (Entidad entidad : rankingEntidades) {
-            Integer posicionEnRankingMayorIncidentesReportados = rankingEntidades.indexOf(entidad);
-            System.out.println("Estas son las posiciones en las que la entidad " + entidad.getNombre() +
-                    " se encuentra en rankingMayorImpactoProblematicas en la posicion: " + posicionEnRankingMayorIncidentesReportados + "º. ");
+            Integer posicionEnRankingMayorIncidentesReportados = rankingEntidades.indexOf(entidad) + 1;
+            System.out.println("Esta es la posicion en las que la entidad " + entidad.getNombre() +
+                    " se encuentra en rankingMayorImpactoProblematicas: " + posicionEnRankingMayorIncidentesReportados + "º. ");
         }
     }
 
