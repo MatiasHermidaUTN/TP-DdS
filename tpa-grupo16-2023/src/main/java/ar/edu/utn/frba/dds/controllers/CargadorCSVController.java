4304beff64fbede18066c265924422e8c691ff8e
Santiagomorel <smorel@frba.utn.edu.ar>
2023-10-15 22:52:51 -0300

    falta terminar la carga de datos

--- tpa-grupo16-2023/src/main/java/ar/edu/utn/frba/dds/controllers/CargadorCSVController.java
+++ tpa-grupo16-2023/src/main/java/ar/edu/utn/frba/dds/controllers/CargadorCSVController.java
@@ -2,8 +2,13 @@ package ar.edu.utn.frba.dds.controllers;
 
 import ar.edu.utn.frba.dds.models.repositorios.RepoEntidad;
 import ar.edu.utn.frba.dds.models.repositorios.RepoOrganismoDeControl;
+import ar.edu.utn.frba.dds.server.Server;
+import io.javalin.config.JavalinConfig;
 import io.javalin.http.Context;
 import io.javalin.http.UploadedFile;
+import io.javalin.util.FileUtil;
+
+import java.io.File;
 
 public class CargadorCSVController {
     private RepoEntidad repoEntidad;
@@ -19,8 +24,8 @@ public class CargadorCSVController {
     }
     public void cargarDatos(Context context) {
         String redirectScript;
-        UploadedFile file = context.uploadedFile("csvFile");
-        if(file == null || file.size() == 0){
+        UploadedFile uploadedFile = context.uploadedFile("csvFile");
+        if(uploadedFile == null || uploadedFile.size() == 0){
             redirectScript = """
                     <script>
                     window.alert(\"No se cargo un archivo, volve a intentar\");
@@ -28,8 +33,10 @@ public class CargadorCSVController {
                     </script>
                     """;
         } else {
-            System.out.println(file.filename());
+            System.out.println(uploadedFile.filename());
+            FileUtil.streamToFile(uploadedFile.content(), "/update" + uploadedFile.filename());
             // aca va la implementacion del lectorCSV pero solamente tengo el nombre del archivo
+
             redirectScript = """
                     <script>
                     window.alert(\"Archivo cargado exitosamente\");
