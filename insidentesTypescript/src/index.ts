714979b1cde9bcbd7a08a356ed26ea3e5b41ac26
MattP2001 <mpereda@frba.utn.edu.ar>
2023-11-10 16:38:38 -0300

    agregamos vistas y un par de cosas

---
+++ insidentesTypescript/src/index.ts
@@ -0,0 +1,29 @@
+import express from 'express';
+import { createConnection } from 'typeorm';
+import { Liquid } from 'liquidjs';
+
+const app = express();
+const port = 3000;
+
+// TypeORM configuration
+createConnection()
+  .then(() => {
+    console.log('Connected to the database');
+  })
+  .catch((error) => console.error('TypeORM connection error: ', error));
+
+// LiquidJS configuration
+const engine = new Liquid();
+app.engine('liquid', engine.express());
+app.set('views', './views');
+app.set('view engine', 'liquid');
+
+// Express route
+app.get('/', (req, res) => {
+  res.render('index', { message: 'Hello, TypeScript!' });
+});
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`);
+});
