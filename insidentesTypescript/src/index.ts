7554f58eab02914d90f8a9c2c598e5317b384e17
Santiagomorel <smorel@frba.utn.edu.ar>
2023-11-26 12:32:02 -0300

    ultimos cambios antes del merge

--- insidentesTypescript/src/index.ts
+++ insidentesTypescript/src/index.ts
@@ -18,3 +18,34 @@ AppDataSource.initialize().then(async () => {
     console.log("Here you can setup and run express / fastify / any other framework.")
 
 }).catch(error => console.log(error))
+
+import express from 'express';
+import { createConnection } from 'typeorm';
+import { Liquid } from 'liquidjs';
+import "reflect-metadata";
+
+const app = express();
+const port = 3000;
+
+// TypeORM configuration
+createConnection()
+  .then(() => {
+    console.log('Connected to the database');
+  })
+  .catch((error) => console.error('TypeORM connection error: ', error));
+
+// LiquidJS configuration
+const engine = new Liquid();
+app.engine('liquid', engine.express());
+app.set('views', './views');
+app.set('view engine', 'liquid');
+
+// Express route
+app.get('/', (req, res) => {
+  res.render('index', { message: 'Hello, TypeScript!' });
+});
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`);
+});
