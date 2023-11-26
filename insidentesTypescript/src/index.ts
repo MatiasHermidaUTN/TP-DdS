704f1ae0039e5aced39b6c3defdffef6d0896f48
Santiagomorel <smorel@frba.utn.edu.ar>
2023-11-25 23:23:18 -0300

    funcionando la base de datos con el entity

--- insidentesTypescript/src/index.ts
+++ insidentesTypescript/src/index.ts
@@ -1,30 +1,20 @@
-import express from 'express';
-import { createConnection } from 'typeorm';
-import { Liquid } from 'liquidjs';
-import "reflect-metadata";
+import { AppDataSource } from "./data-source"
+import { User } from "./entity/User"
 
-const app = express();
-const port = 3000;
+AppDataSource.initialize().then(async () => {
 
-// TypeORM configuration
-createConnection()
-  .then(() => {
-    console.log('Connected to the database');
-  })
-  .catch((error) => console.error('TypeORM connection error: ', error));
+    console.log("Inserting a new user into the database...")
+    const user = new User()
+    user.firstName = "Timber"
+    user.lastName = "Saw"
+    user.age = 25
+    await AppDataSource.manager.save(user)
+    console.log("Saved a new user with id: " + user.id)
 
-// LiquidJS configuration
-const engine = new Liquid();
-app.engine('liquid', engine.express());
-app.set('views', './views');
-app.set('view engine', 'liquid');
+    console.log("Loading users from the database...")
+    const users = await AppDataSource.manager.find(User)
+    console.log("Loaded users: ", users)
 
-// Express route
-app.get('/', (req, res) => {
-  res.render('index', { message: 'Hello, TypeScript!' });
-});
+    console.log("Here you can setup and run express / fastify / any other framework.")
 
-// Start the server
-app.listen(port, () => {
-  console.log(`Server is running at http://localhost:${port}`);
-});
+}).catch(error => console.log(error))
